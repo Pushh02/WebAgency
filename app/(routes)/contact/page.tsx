7614275b46a1ac8986a563bb47b5341a051e72c0
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-07-16 17:58:21 +0530

    fixed contact page bug

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -28,148 +28,148 @@ const Contact = () => {
         <div className="absolute bg-slate-400 -top-4 left-1/2 transform -translate-x-1/2 px-2  rounded-lg w-fit">
           <h1 className="text-lg text-black">Write us to connect</h1>
         </div>
-        <div>
-          {/* form */}
-          <form id="contact" className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-first-name"*/
-                >
-                  Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="grid-name"
-                  type="text"
-                  name="name"
-                  placeholder="Full Name"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label
-                  className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-last-name"*/
-                >
-                  Mobile
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-mobile-no"
-                  type="number"
-                  name="number"
-                  placeholder="Number"
-                />
-              </div>
+        {/* form */}
+        <form id="contact" className="w-fit max-w-lg mx-auto">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-first-name"*/
+              >
+                Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-name"
+                type="text"
+                name="name"
+                placeholder="Full Name"
+              />
             </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-password"*/
-                >
-                  Email
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-email"
-                  type="email"
-                  name="email"
-                  placeholder="abc@xyz.com"
-                />
-              </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-last-name"*/
+              >
+                Mobile
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-mobile-no"
+                type="number"
+                name="number"
+                placeholder="Number"
+              />
             </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-password"*/
-                >
-                  Message
-                </label>
-                <textarea
-                  rows={3}
-                  placeholder="Whats on your mind..."
-                  name="message"
-                  className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                ></textarea>
-              </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-password"*/
+              >
+                Email
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-email"
+                type="email"
+                name="email"
+                placeholder="abc@xyz.com"
+              />
             </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-2">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-white text-lg font-medium mb-2" /*for="grid-password"*/
+              >
+                Message
+              </label>
+              <textarea
+                rows={3}
+                placeholder="Whats on your mind..."
+                name="message"
+                className="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              ></textarea>
+            </div>
+          </div>
 
-            <div className="flex items-center">
-              <input
-                id="link-checkbox"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
-                Website Design and Development
-              </label>
-            </div>
-            <div className="flex items-center">
-              <input
-                id="link-checkbox"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
-                SEO Optimization
-              </label>
-            </div>
-            <div className="flex items-center">
-              <input
-                id="link-checkbox"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
-                ML and AI Services
-              </label>
-            </div>
-            <div className="flex items-center">
-              <input
-                id="link-checkbox"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
-                Website Hosting and Maintenance
-              </label>
-            </div>
+          <div className="flex items-center">
+            <input
+              id="link-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+              Website Design and Development
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="link-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+              SEO Optimization
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="link-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+              ML and AI Services
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="link-checkbox"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label className="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+              Website Hosting and Maintenance
+            </label>
+          </div>
 
-            <div className="flex flex-wrap mt-8 -mb-6 place-content-center">
-              <div className="w-1/3">
-                <button
-                  onClick={handleSubmit}
-                  className="py-2 button-primary text-center text-white cursor-pointer rounded-lg w-full"
-                >
-                  Submit
-                </button>
-              </div>
+          <div className="flex flex-wrap mt-8 -mb-6 place-content-center">
+            <div className="w-1/3">
+              <button
+                onClick={handleSubmit}
+                className="py-2 button-primary text-center text-white cursor-pointer rounded-lg w-full"
+              >
+                Submit
+              </button>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
       {/*  */}
 
       {/*  */}
+
+      {!isOpen && (
+        <MdConnectWithoutContact
+          onClick={() => setIsOpen(true)}
+          id="contact-button"
+          className="cursor-pointer sticky left-[90%] bottom-10 bg-[#824df3ae] rounded-full p-2 shadow-gray-400"
+          size={70}
+        />
+      )}
       <AnimatePresence>
-        {!isOpen ? (
-          <MdConnectWithoutContact
-            onClick={() => setIsOpen(true)}
-            id="contact-button"
-            className="cursor-pointer sticky left-[90%] bottom-10 bg-[#824df3ae] rounded-full p-2 shadow-gray-400"
-            size={70}
-          />
-        ) : (
+        {isOpen && (
           <motion.div
-            initial={{ translateY: 50, opacity: 0 }}
-            whileInView={{ translateY: 0, opacity: 1 }}
-            transition={{ duration: 1, delay: 0.2 }}
-            exit={{ translateY: -50, opacity: 0 }}
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, delay: 0.2 }}
+            exit={{ y: 50, opacity: 0 }}
             id="contact-div"
-            className="hidden w-[22rem] h-fit px-8 py-8 ml-auto bg-[#6639c7a8] rounded-3xl mr-2 absolute bottom-4 right-4"
+            className="w-[22rem] h-fit px-8 py-8 ml-auto bg-[#6639c7a8] rounded-3xl mr-2 sticky left-[90%] bottom-10"
           >
             <IoCloseCircle
               onClick={() => setIsOpen(false)}
