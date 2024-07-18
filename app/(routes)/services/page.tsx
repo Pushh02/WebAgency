74b2052ddff5f50f38779ad36b7c55621c071b20
Pushkar Kamble <pushkarkamble23@gmail.com>
2024-07-18 10:43:00 +0530

    added props to services page

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,13 +1,21 @@
 import ServicesPageCard from "@/components/services/ServicesPageCard";
+import services from "../../../data/services.json";
 
 const Services = () => {
   return (
     <div className="my-8">
       <h1 className="text-4xl text-center mb-8">Our Services</h1>
       <div className="flex flex-col justify-center items-center gap-y-8">
-        <ServicesPageCard />
-        <ServicesPageCard />
-        <ServicesPageCard />
+        {services.map((services, index) => {
+          return (
+            <ServicesPageCard
+              key={index}
+              img={services.img}
+              title={services.title}
+              description={services.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- components/services/ServicesPageCard.tsx
+++ components/services/ServicesPageCard.tsx
@@ -1,23 +1,25 @@
-import { StaticImageData } from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-interface ServicesPageCardProps{
-    title: string,
-    description: string,
-    img: StaticImageData,
+interface ServicesPageCardProps {
+  title: string;
+  description: string;
+  img: string;
 }
 
-const ServicesPageCard = () => {
+const ServicesPageCard = ({
+  title,
+  description,
+  img,
+}: ServicesPageCardProps) => {
   return (
     <div className="group w-4/5 h-60 bg-[#d9d9d960] flex items-center rounded-md relative">
-      <div className="w-48 h-48 bg-slate-300 ml-4 flex-none rounded-md absolute left-0 group-hover:-left-12 transition-all duration-500" />
+      <img src={img} alt={title} className="w-48 h-48 bg-slate-300 ml-4 flex-none rounded-md absolute left-0 group-hover:-left-12 transition-all duration-500" />
       <div className="mx-4">
         <h3 className="text-2xl absolute top-4 right-4 group-hover:-top-4 group-hover:-right-4 group-hover:bg-slate-300 px-2 rounded-md group-hover:text-black transition-all duration-500">
-          TitleTitleTitle
+          {title}
         </h3>
         <p className="ml-52 group-hover:ml-44 group-hover:text-xl transition-all duration-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          ipsa id sapiente et quam exercitationem reiciendis ducimus provident
-          libero, nobis minima distinctio in dicta.
+          {description}
         </p>
       </div>
     </div>
